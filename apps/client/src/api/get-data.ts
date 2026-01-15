13e25ff433c2a67b4f42665cb970316deb5a9d32
Nikita Zhidov <nzhidov02@gmail.com>
2026-01-15 21:27:51 +0200

    Add deploy

--- apps/client/src/api/get-data.ts
+++ apps/client/src/api/get-data.ts
@@ -1,5 +1,6 @@
 export async function getData(): Promise<{ message: string }> {
-  const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  const url = process.env.API_URL || 'http://localhost:3001';
+
   const data = await fetch(`${url}/api`, { cache: 'no-store' });
   const json = await data.json();
 
